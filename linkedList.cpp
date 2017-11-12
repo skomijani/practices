95b8ffd50bf8760b145cac9a46a0feb190449a33
Saeedeh Komijani <skomijan@gmail.com>
2017-11-12 10:51:33 -0800

    Update linkedList.cpp

--- linkedList.cpp
+++ linkedList.cpp
@@ -1,3 +1,20 @@
+Node *createLinkedList()
+{
+	Node *head = NULL;
+	int num_of_elements;
+	int value;
+	printf("Enter the number of linklist nodes: ");
+	scanf("%d", &num_of_elements);
+	printf("\nEnter linklist node values: ");
+	for (int i = 0; i < num_of_elements; i++)
+	{
+		scanf("%d", &value);
+		Node *tempNode = newNode(value);
+		insertNode_sequential(&head, tempNode);
+	}
+	return head;
+}
+
 
 void removeDups()
 {
@@ -56,4 +73,54 @@ void deleteMiddleNode()
 	prev->next = firstPtr->next;
 	delete tempNode;
 	printLinklist(head);
+}
+
+///
+
+void partitionLinkedlist()
+{
+	Node * head = createLinkedList();
+	printLinklist(head);
+	printf("\nprint value X: ");
+	int x;
+	scanf("%d", &x);
+	Node *Shead = NULL;
+	Node *Stail = NULL;
+	Node *Ghead = NULL;
+	Node *Gtail = NULL;
+
+	Node *curNode = head;
+	while (curNode != NULL)
+	{
+		if (curNode->value < x)
+		{
+			if (Shead != NULL)
+			{
+				Stail->next = curNode;
+				Stail = Stail->next;
+			}
+			else
+			{
+				Shead = curNode;
+				Stail = curNode;
+			}
+		}
+		else
+		{
+			if (Ghead != NULL)
+			{
+				Gtail->next = curNode;
+				Gtail = Gtail->next;
+			}
+			else
+			{
+				Ghead = curNode;
+				Gtail = curNode;
+			}
+		}
+		curNode = curNode->next;
+	}
+	Stail->next = Ghead;
+	Gtail->next = NULL;
+	printLinklist(Shead);
 }
